300225778b34ea033032c7fe3ff3f1f0a4c91ec5
fish <chuanxing@mamahao.com>
2019-10-29 13:38:23 +0800

    滑动窗口算法

--- leetcode/leetcode/leetcode3/Solution.swift
+++ leetcode/leetcode/leetcode3/Solution.swift
@@ -13,24 +13,60 @@ class Solution {
         var result:String = ""
         var temp:String = ""
         
-        for item in s.characters {
+        for item in s {
             let startPlace = s.index(of: item)
             let subString = s[startPlace!..<s.endIndex]
             temp = self.tempReuslt(s: String.init(subString))
-            result = temp.characters.count > result.characters.count ? temp : result
+            result = temp.count > result.count ? temp : result
         }
-        return result.characters.count
+        return result.count
     }
     
     func tempReuslt(s:String) -> String {
         var result : String = ""
-        for i in s.characters {
+        for i in s {
             if(result.contains(i)) {
                 break
             }
-            result.characters.append(i)
+            result.append(i)
         }
         return result
     }
     
 }
+
+
+//滑动窗口
+
+
+class Solution1 {
+    class LongestSubstringWithoutRepeatingCharacters {
+        func lengthOfLongestSubstring(_ s: String) -> Int {
+            var longest = 0, left = 0, set = Set<Character>()
+            let sChars = Array(s)
+            
+            for (i, char) in sChars.enumerated() {
+                if set.contains(char) {
+                    
+                    //计算最大长度
+                    longest = max(longest, i - left)
+                    
+                    //把重复字符以及它左边的都移出窗口
+                    while sChars[left] != char {
+                        set.remove(sChars[left])
+                        left += 1
+                    }
+                    left += 1
+                    
+                } else {
+                    set.insert(char)
+                }
+            }
+            
+            return max(longest, sChars.count - left)
+        }
+    }
+}
+
+
+
